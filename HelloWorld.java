e3a3416631eae8d464f85a400e8f76e478c35033
Chiru <chirudevops3@gmail.com>
2020-07-17 03:12:46 +0000

    add to git

--- HelloWorld.java
+++ HelloWorld.java
@@ -33,7 +33,8 @@
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
+  public static void main(String[] args) {  
+  System.out.println("Hello, World");
+  System.out.println("Hello, Devops");
   }
 }
